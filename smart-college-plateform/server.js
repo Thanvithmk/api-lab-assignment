51e73df13f747d5b2d255412a72f822836fbae41
Thanvith Manjunath K <thanvithmk@gmail.com>
2026-03-02 15:51:13 +0530

    post,put ,delete working

--- smart-college-plateform/server.js
+++ smart-college-plateform/server.js
@@ -11,7 +11,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-
 let users=[
     {id:1,name:"Thanvith Manjunath k",email:"thanvith@gmail.com"},
     {id:2,name:"NEW HORIZON",email:"nhce@gmail.com"}
@@ -31,6 +30,33 @@ app.get("/users/:id",(req,res)=>{
         res.status(404).json({message:"User not found"});
     }
 })
+
+app.post("/users",(req,res)=>{
+    const newUser=req.body;
+    users.push(newUser);
+    res.status(201).json(newUser);      
+})
+
+app.put("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const user = users.find(u => u.id == id);
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    user.name = req.body.name;
+    user.email = req.body.email;
+    res.status(200).json(user);
+});
+
+app.delete("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const userIndex = users.findIndex(u => u.id == id);
+    if (userIndex === -1) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    users.splice(userIndex, 1);
+    res.status(204).send();
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
